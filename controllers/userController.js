a1adfff8604461e16105f64ed1b0f9c08e81c5fd
Mr-Invinsible <architsrivastava945@gmail.com>
2025-09-06 11:51:33 +0530

    setup user model and working registration API

---
+++ controllers/userController.js
@@ -0,0 +1,46 @@
+import User from '../models/userModels.js';
+import jwt from 'jsonwebtoken';
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      res.status(400);
+      throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    if (user) {
+      res.status(201).json({ 
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error('Invalid user data');
+    }
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+
+export { registerUser };
